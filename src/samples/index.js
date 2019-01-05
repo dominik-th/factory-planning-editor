3e59904945f89726c4d42b51088b6bb4839e9f25
Dominik Thiemermann <dominik@dthiemermann.org>
2019-01-05 21:30:19 +0100

    Put samples in separate files

---
+++ src/samples/index.js
@@ -0,0 +1,8 @@
+import cyclicInformationState from './cyclic-information.json';
+
+export default {
+  cyclicInformation: {
+    title: 'Cyclic Information Flow',
+    state: cyclicInformationState
+  }
+}
