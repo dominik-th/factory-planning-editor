4b58089df2366ce15c4debcb3182d18e2fc52b0f
Dominik Thiemermann <dominik@dthiemermann.org>
2019-02-20 14:44:32 +0100

    Add modeling to excel export

--- src/helpers/excel.js
+++ src/helpers/excel.js
@@ -6,17 +6,20 @@ export async function generateExcelSheet(state) {
   let workbook = new ExcelJS.Workbook();
   let worksheet = workbook.addWorksheet('FPE-Export');
 
+  // make enough space for long titles
   worksheet.getRow(1).height = 200;
   worksheet.getColumn(1).width = 50;
 
   let informationIds = Object.keys(state.informationTypes);
   let moduleIds = Object.keys(state.planningModules);
 
+  // write all information types in the first row
   for (let [index, informationId] of informationIds.entries()) {
     let cell = worksheet.getRow(1).getCell(index + 2);
     cell.value = state.informationTypes[informationId].name;
     cell.alignment = { textRotation: 90 };
   }
+  // fill the rest with modules in the first column and information types in the matrix
   for (let [index, moduleId] of moduleIds.entries()) {
     let moduleRow = worksheet.getRow(index + 2);
     moduleRow.getCell(1).value = state.planningModules[moduleId].name;
@@ -28,7 +31,7 @@ export async function generateExcelSheet(state) {
       cell.fill = {
         type: 'pattern',
         pattern: 'solid',
-        fgColor: { argb: '00A9D08E' } // green
+        fgColor: { argb: '00A9D08E' } // light green
       };
     }
     for (let outId of module.outputInformation) {
@@ -43,9 +46,57 @@ export async function generateExcelSheet(state) {
     }
   }
 
+  // lock first row and column
   worksheet.views = [
     { state: 'frozen', xSplit: 1, ySplit: 1, topLeftCell: 'B2' }
   ];
+
+  // second worksheet for modeling modules
+  let modelingModulesWs = workbook.addWorksheet('Modeling-Modules');
+  // break down object in separate columns
+  modelingModulesWs.getRow(1).getCell(1).value =
+    'Module (referencing row number)';
+  modelingModulesWs.getRow(1).getCell(2).value = 'Position X';
+  modelingModulesWs.getRow(1).getCell(3).value = 'Position Y';
+  modelingModulesWs.getRow(1).getCell(4).value = 'Num. Employees';
+  modelingModulesWs.getRow(1).getCell(5).value = 'Cost (in cents)';
+  modelingModulesWs.getRow(1).getCell(6).value = 'Duration (in minutes)';
+  modelingModulesWs.getRow(1).getCell(7).value = 'Custom';
+  let modelingModuleIds = Object.keys(state.modeling.modules);
+  for (let [index, modelingModuleId] of modelingModuleIds.entries()) {
+    let modelingModule = state.modeling.modules[modelingModuleId];
+    modelingModulesWs.getRow(2 + index).getCell(1).value =
+      moduleIds.indexOf(modelingModule.moduleId) + 2;
+    modelingModulesWs.getRow(2 + index).getCell(2).value =
+      modelingModule.position.x;
+    modelingModulesWs.getRow(2 + index).getCell(3).value =
+      modelingModule.position.y;
+    modelingModulesWs.getRow(2 + index).getCell(4).value =
+      modelingModule.attributes.numEmployees;
+    modelingModulesWs.getRow(2 + index).getCell(5).value =
+      modelingModule.attributes.cost;
+    modelingModulesWs.getRow(2 + index).getCell(6).value =
+      modelingModule.attributes.duration;
+    modelingModulesWs.getRow(2 + index).getCell(7).value = JSON.stringify(
+      modelingModule.attributes.custom
+    );
+  }
+
+  // thirs worksheet for connection between modeling modules
+  let modelingLinksWs = workbook.addWorksheet('Modeling-Links');
+  modelingLinksWs.getRow(1).getCell(1).value = 'From (row number)';
+  modelingLinksWs.getRow(1).getCell(2).value = 'To (row number)';
+  modelingLinksWs.getRow(1).getCell(3).value = 'Connecting Information';
+  let modelingLinkIds = Object.keys(state.modeling.links);
+  for (let [index, modelingLinkId] of modelingLinkIds.entries()) {
+    let modelingLink = state.modeling.links[modelingLinkId];
+    modelingLinksWs.getRow(2 + index).getCell(1).value =
+      modelingModuleIds.indexOf(modelingLink.fromModule) + 2;
+    modelingLinksWs.getRow(2 + index).getCell(2).value =
+      modelingModuleIds.indexOf(modelingLink.toModule) + 2;
+    modelingLinksWs.getRow(2 + index).getCell(3).value =
+      informationIds.indexOf(modelingLink.informationId) + 2;
+  }
 
   let mimeType =
     'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
