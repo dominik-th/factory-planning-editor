11f5b9afde20b6a0fc44ab9d25a627bec0d1b667
Dominik Thiemermann <dominik@dthiemermann.org>
2019-03-03 19:41:17 +0100

    Add floating action button component

---
+++ src/components/FloatingActionButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="floating-action-button">
+    <font-awesome-icon :icon="icon" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FloatingActionButton',
+  props: {
+    icon: {
+      type: String,
+      default: 'bars'
+    }
+  }
+};
+</script>
+
+<style scoped>
+.floating-action-button {
+  background-color: #40a73f;
+  color: #fff;
+  box-shadow: 2px 2px 4px #999;
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  border-radius: 25px;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  cursor: pointer;
+  transition: all linear 0.1s;
+}
+.floating-action-button:hover {
+  background-color: #007206;
+}
+</style>
